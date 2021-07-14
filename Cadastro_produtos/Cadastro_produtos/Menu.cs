e397d835a32616674141f345b4f145fd20876f2e
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-07-14 16:06:57 -0300

    Add menu com cadastro produtos

---
+++ Cadastro_produtos/Cadastro_produtos/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_produtos
+{
+    class Menu
+    {
+        List<Produto> produtos = new List<Produto>();
+        List<Categorias> categorias = new List<Categorias>();
+
+        int countProduto = 1;
+        public void CadastrarProduto()
+        {
+            Console.Write("Digite um Produto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o Valor do produto: ");
+            decimal valor = Convert.ToDecimal(Console.ReadLine());
+
+            Produto produto = new Produto(countProduto, nome, valor);
+            countProduto++;
+
+            produtos.Add(produto);
+        }
+
+    }
+}
