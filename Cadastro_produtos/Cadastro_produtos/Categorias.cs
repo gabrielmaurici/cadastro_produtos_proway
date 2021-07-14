91c0c52b5fbc356309aa08735797245efe3afa36
Eduardo Weber <edu.weber07@gmail.com>
2021-07-14 15:40:16 -0300

    add Categorias

--- Cadastro_produtos/Cadastro_produtos/Categorias.cs
+++ Cadastro_produtos/Cadastro_produtos/Categorias.cs
@@ -6,21 +6,19 @@ using System.Threading.Tasks;
 
 namespace Cadastro_produtos
 {
-    class Categoria
+    class Categorias
     {
-
         int id = 0;
-        
+
         public string Nome { get; set; }
 
         public string Descricao { get; set; }
 
-        public Categoria(int id, string nome, string descricao)
+        public Categorias(int id, string nome, string descricao)
         {
             this.id = id;
             this.Nome = nome;
             this.Descricao = descricao;
         }
-
     }
 }
